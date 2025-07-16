d5533ef28a255a9190f8f2c0df4dec111a3c58ef
Edgar Rosende <rosendegomezedgar@gmail.com>
2025-07-17 01:26:19 +0200

    client data

--- src/components/participants/ParticipantData.tsx
+++ src/components/participants/ParticipantData.tsx
@@ -1,6 +1,6 @@
 import type { Participant } from "../../interfaces/Participant";
 import './ParticipantData.css';
-import { Divider } from "@mantine/core";
+import { Divider, TextInput } from "@mantine/core";
 
 const ParticipantData = ({ participant }: { participant: Participant | null }) => {
   return (
@@ -26,8 +26,7 @@ const ParticipantData = ({ participant }: { participant: Participant | null }) =
 const LabelValue = ({ className, label, value }: { className: string, label: string, value: string | undefined }) => {
   return (
     <div className={className}>
-      <p className="label">{label}: </p>
-      <p className="value">{value}</p>
+      <TextInput className="value" onSelect={() => {}} readOnly label={label} value={value} />
     </div>
   )
 }
