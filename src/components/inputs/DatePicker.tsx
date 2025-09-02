a44dc560dc3dcdff06b5faa7321949067433764e
erosende <rosendegomezedgar@gmail.com>
2025-09-02 16:31:18 +0200

    fixed datepicker bug

--- src/components/inputs/DatePicker.tsx
+++ src/components/inputs/DatePicker.tsx
@@ -2,20 +2,36 @@ import "./LabelInputs.css";
 import "./DatePicker.css";
 
 const DatePicker = (
-  { 
+  {
     label,
-    date, 
+    date,
     setDate
-  } : { 
+  }: {
     label: string,
-    date: Date | null, 
-    setDate: (date: Date | null) => void 
+    date: Date | null,
+    setDate: (date: Date | null) => void
   }) => {
+
+  function handleDateChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value === '') {
+      setDate(null);
+      return;
+    }
+    setDate(new Date(e.target.value));
+  }
+
+  function formatDate(date: Date | null) {
+    if (!date) {
+      return '';
+    }
+    return date.toISOString().split('T')[0];
+  }
+
 
   return (
     <div className="date-picker-container">
       <p className="label">{label}</p>
-      <input className="value" type="date" value={date?.toISOString().split('T')[0]} onChange={(e) => setDate(new Date(e.target.value))} />
+      <input className="value" type="date" value={formatDate(date)} onChange={handleDateChange} />
     </div>
   )
 
